c91c59e6ec05f199a50eb7998ef6f47e60c57a16
adimo85 <adimo.developer@gmail.com>
2015-07-23 20:30:48 +0200

    v3

--- ejercicio/express-ejercicio/routes/index.js
+++ ejercicio/express-ejercicio/routes/index.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var router = express.Router();
+var quizController = require('../controllers/quiz_controller');
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -7,3 +9,9 @@ router.get('/', function(req, res, next) {
 });
 
 module.exports = router;
+
+
+router.get('/quizes/question',quizController.question);
+router.get('/quizes/answer', quizController.answer);
+// router.get('/author', function(req, res){
+	// res.render('author');});
